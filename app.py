b4e13a576e8c9c70f9bd7269ac1bfd11208ccd89
eldaputri <140079288+eldaputri@users.noreply.github.com>
2024-10-30 23:36:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,36 @@ from sklearn.preprocessing import StandardScaler
 st.title("Aplikasi Prediksi Penyakit Anemia")
 
 # Input Data
+import os
+os.system('pip install scikit-learn')
+
+import streamlit as st
+from sklearn.model_selection import train_test_split  # Import yang sebelumnya menyebabkan error
+import pandas as pd
+
+# Sidebar for input parameters
 st.sidebar.header("Input Parameter")
+
 def user_input_features():
-  gender = st.sidebar.selectbox("Gender"(0, 1))
-  hemogolobin = st.number_input("Hemogologi")
-  mch = st.sidebar.number_input("MCH")
-  mchc = st.sidebar.number_input("MCHC")
-  mcv = st.sidebar.number_input("MCV")
-  result = st.sidebar.selectbox("Result", (0, 1))
+    gender = st.sidebar.selectbox("Gender", (0, 1))
+    hemoglobin = st.sidebar.number_input("Hemoglobin")
+    mch = st.sidebar.number_input("MCH")
+    mchc = st.sidebar.number_input("MCHC")
+    mcv = st.sidebar.number_input("MCV")
+    result = st.sidebar.selectbox("Result", (0, 1))
+    
+    # Creating a dictionary of user inputs
+    data = {
+        'gender': gender,
+        'hemoglobin': hemoglobin,
+        'mch': mch,
+        'mchc': mchc,
+        'mcv': mcv,
+        'result': result
+    }
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+# Display user input features
+df = user_input_features()
+st.write(df)
